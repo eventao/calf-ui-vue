77f3c0cbaf729ecca59c571efec19ac55cbb78bf
paopao0218 <1643740208@qq.com>
2018-12-26 17:56:22 +0800

    初始化!

---
+++ src/components/carousel/CCarouselItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="c-carousel-i" ref="carouselItem" :style="itemStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CCarouselItem',
+    props: {},
+    data() {
+      return {
+        itemStyle: {
+          marginLeft: '0px',
+        }
+      };
+    },
+    mounted() {
+      let w = this.$refs.carouselItem.clientWidth;
+      this.itemStyle.marginLeft = `-${w / 2}px`;
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped lang="less">
+  .c-carousel-i {
+    width: 80%;
+    min-height: 100%;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    color: #fff;
+    overflow: hidden;
+    transition: transform 0.5s, opacity 0.5s;
+    &:hover {
+      opacity: 1 !important;
+    }
+  }
+</style>
+
+
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="carousel" ref="carousel" :style="carouselStyle" @click="carouseClick($event)">
+    <slot></slot>
+    <div class="go p" @click="prev()">
+      前
+    </div>
+    <div class="go n" @click="next()">
+      后
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'CCarousel',
+    props: {
+      interval: Number,
+      height: String,
+      value: Number,
+    },
+    data() {
+      return {
+        carouselStyle: {
+          height: 'auto',
+          minHeight: '430px'
+        },
+        currentIndex: 0,
+        total: 0,
+        carouselTimeout:null,
+      };
+    },
+    mounted() {
+      let that = this;
+      if (that.height) {
+        that.carouselStyle.height = this.height;
+        that.carouselStyle.minHeight = this.height;
+      }
+      that.init();
+    },
+    methods: {
+      init() {
+        let that = this;
+        that.$children.forEach((el, i) => {
+          el.$el.style.zIndex = i + 1;
+        });
+        this.total = that.$children.length;
+
+        if (this.interval > 0) {
+          this.runAuto();
+        }
+        this.setCarouselChange();
+      },
+      next(cb) {
+        this.resetPrev();
+        if (this.currentIndex < this.total - 1) {
+          this.currentIndex++;
+        } else {
+          this.currentIndex = 0;
+        }
+        this.setCarouselChange();
+        this.$emit('input', this.currentIndex);
+        if(cb && typeof cb === 'function')cb();
+        clearTimeout(this.carouselTimeout);
+        this.runAuto();
+      },
+      prev() {
+        this.resetPrev();
+        if (this.currentIndex > 0) {
+          this.currentIndex--;
+        } else {
+          this.currentIndex = this.total - 1;
+        }
+        this.setCarouselChange();
+        this.$emit('input', this.currentIndex);
+
+        clearTimeout(this.carouselTimeout);
+        this.runAuto();
+
+      },
+      resetPrev() {
+        let that = this;
+        that.$children.forEach(item => {
+          item.$el.style.transform = 'translateX(0) scaleY(1)';
+          item.$el.style.opacity = '0';
+          item.$el.style.zIndex = '2';
+        });
+      },
+      setCarouselChange() {
+        let that = this, cl = that.$children.length,
+          max = cl - 1,
+          currI = max - this.currentIndex,
+          currEle = that.$children[currI];
+        currEle.$el.style.transform = 'translateX(0) scale(1)';
+        currEle.$el.style.opacity = '1';
+        currEle.$el.style.zIndex = cl + 1;
+
+        let prev = currI >= max ? 0 : currI + 1;
+        let next = currI <= 0 ? max : currI - 1;
+        let conWidth = this.$refs.carousel.clientWidth;
+        let preEle = that.$children[prev].$el;
+        let nEle = that.$children[next].$el;
+        let subW = (conWidth - preEle.clientWidth) / 2;
+
+        preEle.style.transform = `translateX(-${subW}px) scaleY(0.8)`;
+        preEle.style.opacity = 0.6;
+        preEle.style.zIndex = 2;
+
+        nEle.style.transform = `translateX(${subW}px) scaleY(0.8)`;
+        nEle.style.opacity = 0.6;
+        nEle.style.zIndex = 2;
+
+      },
+      runAuto() {
+        this.carouselTimeout = setTimeout(() => {
+          this.next(() => {
+            this.runAuto();
+          });
+        }, this.interval);
+      },
+      carouseClick(e){
+        let param = e;
+        let w = this.$refs.carousel.clientWidth;
+        if(param.layerX / w > 0.6){
+          this.next();
+        }else if(param.layerX / w < 0.4){
+          this.prev();
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .carousel {
+    width: 100%;
+    min-height: 430px;
+    height: auto;
+    position: relative;
+    .go{
+      width: 0.20rem;
+      height: 0.20rem;
+      top: 50%;
+      margin-top: -0.1rem;
+      border-radius: 50%;
+      font-size: .12rem;
+      background-color:rgba(0,0,0,0.3);
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      color:#fff;
+      position:absolute;
+      z-index:1;
+      cursor:pointer;
+      user-select: none;
+      &.p{
+        left:.15rem;
+      }
+      &.n{
+        right:.15rem;
+      }
+    }
+  }
+</style>
+
+
